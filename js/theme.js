a218352a43c8c0bf0a0340591a6cada643ee7280
machonaleks2 <machonaleks2@gmail.com>
2024-12-05 01:08:58 +0100

    almostdonewithstructure

---
+++ js/theme.js
@@ -0,0 +1,65 @@
+
+      (function ($) {"use strict";
+      
+      $(function () {
+        var header = $(".start-style");
+        $(window).scroll(function () {
+          var scroll = $(window).scrollTop();
+    
+          if (scroll >= 10) {
+            header.removeClass('start-style').addClass("scroll-on");
+          } else {
+            header.removeClass("scroll-on").addClass('start-style');
+          }
+        });
+      });
+    
+      //Animation
+    
+      $(document).ready(function () {
+        $('body.hero-anime').removeClass('hero-anime');
+      });
+    
+      //Menu On Hover
+    
+      $('body').on('mouseenter mouseleave', '.nav-item', function (e) {
+        if ($(window).width() > 750) {
+          var _d = $(e.target).closest('.nav-item');_d.addClass('show');
+          setTimeout(function () {
+            _d[_d.is(':hover') ? 'addClass' : 'removeClass']('show');
+          }, 1);
+        }
+      });
+    
+    
+    
+    })(jQuery);
+
+// Blank Target External Links
+$(document.links).filter(function() {
+return this.hostname != window.location.hostname;
+}).attr('target', '_blank');
+
+// lbox
+
+const lightbox = document.createElement('div');
+lightbox.id = 'lightbox';
+document.body.appendChild(lightbox);
+
+const images = document.querySelectorAll('.article-post img');
+images.forEach(image => {
+    image.addEventListener('click', e => {
+        lightbox.classList.add('active');
+        const img = document.createElement('img');
+        img.src = image.src;
+        while (lightbox.firstChild) {
+            lightbox.removeChild(lightbox.firstChild);
+        }
+        lightbox.appendChild(img);
+        lightbox.style.setProperty('cursor', 'zoom-out');
+    });
+});
+
+lightbox.addEventListener('click', e => {
+    lightbox.classList.remove('active');
+});
